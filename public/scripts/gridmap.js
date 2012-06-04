ba21d34fde3c43180417aa7b90004194fad0e047
Nick Doiron <ndoiron@mapmeld.com>
2012-06-04 14:26:50 -0700

    use bing maps layer

--- public/scripts/gridmap.js
+++ public/scripts/gridmap.js
@@ -70,12 +70,12 @@ function init(rep){
   cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18, attribution: cloudmadeAttribution}); */
   var bing_key = "Arc0Uekwc6xUCJJgDA6Kv__AL_rvEh4Hcpj4nkyUmGTIx-SxMd52PPmsqKbvI_ce";
   var bing = new L.TileLayer.Bing(bing_key, 'AerialWithLabels', {minZoom:10, maxZoom:19});
-  map.addLayer(bing);
 
   /* initialize the map div and center it */
   map = new L.Map('map');
   cityll = new L.LatLng(response.lat * 1.0, response.lng * 1.0);
-  map.setView(cityll, response.zoom ).addLayer(cloudmade);
+  map.setView(cityll, response.zoom );//.addLayer(cloudmade);
+  map.addLayer(bing);
 
   /* add GeoJSON of wards / county boundary */ 
   $("#wardLayer").checked = true;
